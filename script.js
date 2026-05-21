74c0ae572781f41057665ef010398d7ae7c4f12e
Majharul Islam <666majharulislam@gmail.com>
2026-05-22 02:05:56 +0600

    add toastify

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 const inputBox = document.getElementById('inputBox')
 const todoList = document.getElementById('todoList')
 
-function addTask(){
-    if (inputBox.value === "") {
-        const inputBox = document.getElementById('inputBox')
-const todoList = document.getElementById('todoList')
-
 function addTask() {
     if (inputBox.value === "") {
         Toastify({
@@ -29,34 +24,6 @@ function addTask() {
         li.appendChild(span);
         
         saveData();
-    }
-}
-
-todoList.addEventListener('click' , function(e) {
-    if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
-        saveData()
-    }
-},false)
-
-function saveData() {
-    localStorage.setItem('data' , todoList.innerHTML)
-}
-
-function showTask() {
-    todoList.innerHTML = localStorage.getItem('data')
-}
-
-showTask()
-    } else {
-        let li = document.createElement('li');
-        li.innerHTML = inputBox.value;
-        todoList.appendChild(li);
-        inputBox.value = ""
-        let span = document.createElement('span')
-        span.innerHTML = '\u00d7'
-        li.appendChild(span)
-        saveData()
     }
 }
 
